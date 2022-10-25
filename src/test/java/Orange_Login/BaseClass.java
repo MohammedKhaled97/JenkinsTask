ef70b26a590e7b7eb544a2317d50d1b31275a112
MohammedKhaled97 <108574945+MohammedKhaled97@users.noreply.github.com>
2022-10-25 09:23:37 +0200

    Add files via upload

---
+++ src/test/java/Orange_Login/BaseClass.java
@@ -0,0 +1,58 @@
+package Orange_Login;
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.interactions.Actions;
+import org.testng.annotations.*;
+
+public class BaseClass {
+    static ThreadLocal<WebDriver> drivers = new ThreadLocal<WebDriver>();
+    public static WebDriver driver;
+    public static String browsersc;
+   public static Actions action;
+
+    public static WebDriver getDriver() {
+        return drivers.get();
+    }
+
+
+    @BeforeMethod
+    @Parameters(value = {"browser"})
+    public void open_browser(String browser) throws InterruptedException {
+        browsersc=browser;
+
+        if(browser.equals("Chrome")) {
+            //Driver Configuration
+            WebDriverManager.chromedriver().setup();
+            driver = new ChromeDriver();
+            drivers.set(driver);
+            action = new Actions(driver);
+            //Navigation Into Link
+            getDriver().navigate().to(Login_Page.LoginPageURL);
+            //Maximize the window
+            getDriver().manage().window().maximize();
+            Thread.sleep(1000);
+        }
+        else if (browser.equals("Firefox")) {
+            //Driver Configuration
+            WebDriverManager.firefoxdriver().setup();
+            driver = new FirefoxDriver();
+            drivers.set(driver);
+            //Navigation Into Link
+            getDriver().navigate().to(Login_Page.LoginPageURL);
+            //Maximize the window
+            getDriver().manage().window().maximize();
+            Thread.sleep(1000);
+        }
+    }
+    @AfterMethod
+    public void aftertest() throws InterruptedException {
+        //Quit Driver will close all tabs
+        getDriver().quit();
+
+    }
+
+
+
+}
